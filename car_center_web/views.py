28653db172d54127c49ce10814b98f741dab4d7c
Nikolay Mihailov <hardsky@yandex.ru>
2018-08-26 19:35:46 +0300

    post car details

--- car_center_web/views.py
+++ car_center_web/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django_tables2 import RequestConfig
 from .tables import CarsTable
@@ -24,5 +25,19 @@ def cars(request):
     return render(request, 'cars.html', {'cars': table})
 
 def car_detail(request, id):
-    form = CarForm(initial = {'id': 1, 'name': 'Test1'})
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = CarForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return HttpResponseRedirect('/thanks/')
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = CarForm(initial = {'id': 1, 'name': 'Test1'})
+
     return render(request, 'car_detail.html',{'form': form})
